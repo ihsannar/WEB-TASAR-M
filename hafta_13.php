80aa26fb42dc087fd137d808e92621c0e6d7f8d0
ihsannar <ihsan--2002@hotmail.com>
2024-12-20 11:08:28 +0300

    Update hafta_13.php

--- hafta_13.php
+++ hafta_13.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form İşlemleri</title>
-</head>
+<head>
 <body>
     <h1>Kişi Veritabanı İşlemleri</h1>
 
